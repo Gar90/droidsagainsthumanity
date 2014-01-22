26c9c70eca8cb2e2def6f0e6c5db79e47443ae67
Garret Ferreira <ferreiragarret@gmail.com>
2014-01-21 22:09:19 -0500

    Moved source files to src/main/java which is the typical default for build tools. Removed eclipse workspace specific .classpath and .project files

--- gen/main/java/droidsagainsthumanity/BuildConfig.java
+++ gen/main/java/droidsagainsthumanity/BuildConfig.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package com.example.droidsagainsthumanity;
+package main.java.droidsagainsthumanity;
 
 /* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
 public final class BuildConfig {
